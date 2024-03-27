3ee131670021c190a1e7e5ffa9bb48e46e1d83d7
aidross <rossam06@pfw.edu>
2024-03-27 16:05:44 -0400

    test

--- src/main/java/org/example/Elevator.java
+++ src/main/java/org/example/Elevator.java
@@ -80,17 +80,4 @@ public class Elevator {
         return false;
     }
 
-    public static void main(String[] args) {
-        Elevator elevator = new Elevator(10); // Create an elevator with 10 floors
-
-        elevator.requestFloor(5, true); // Request 5th floor going up
-        elevator.requestFloor(2, false); // Request 2nd floor going down
-        elevator.requestFloor(8, true); // Request 8th floor going up
-
-        elevator.moveElevator(); // Move the elevator
-
-        elevator.requestFloor(1, false); // Request 1st floor going down (from floor 8)
-
-        elevator.moveElevator(); // Move the elevator again
-    }
 }
